4efcae477fea61be253b8259082ea7061e466cd5
nataliekarri <nataliekarri@gmail.com>
2022-08-06 00:04:04 +0300

    Zadanie5

--- Program.cs
+++ Program.cs
@@ -49,16 +49,27 @@
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+
+// while (i <= number)
+// {
+//     if (i%2 == 0)
+//     {
+//         Console.Write(i + ",");
+//     }
+//     i++;
+// }
+ 
+//  Задача 5: Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-int i = 1;
+int i = -number;
 
 while (i <= number)
 {
-    if (i%2 == 0)
-    {
-        Console.Write(i + ",");
-    }
+    Console.Write(i + ",");
     i++;
 }
- 
