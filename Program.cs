687022f09c6312107f123dc01074a77e7cba66c8
nataliekarri <nataliekarri@gmail.com>
2022-08-05 23:03:28 +0300

    update gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
